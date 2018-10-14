938f98e2d4f4c96ad8a641c888834b1f630e0ca0
Thigeshkumar <44030505+Thigeshkumar@users.noreply.github.com>
2018-10-14 21:36:14 +0530

    To check in the local repository

--- thigesh.java
+++ thigesh.java
@@ -3,6 +3,8 @@ class Hello
 Public static void main(String args[])
 {
 System.out.println("Hello world");
+
 System.out.println("Now i am editing this");
+  System.out.println("checking in the local repository");
 
 }
